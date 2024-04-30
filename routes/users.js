cdb174a1303e92d9639fb7c8e27993628110f953
Kostek <ksytnik73kwer@gmail.com>
2024-04-30 21:36:47 +0200

    Add user routes in routes/users.js

---
+++ routes/users.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const router = express.Router();
+
+let users = [
+    {
+        firstName: "John",
+        lastName: "wick",
+        email: "johnwick@gamil.com",
+        DOB: "22-01-1990",
+    },
+    {
+        firstName: "John",
+        lastName: "smith",
+        email: "johnsmith@gamil.com",
+        DOB: "21-07-1983",
+    },
+    {
+        firstName: "Joyal",
+        lastName: "white",
+        email: "joyalwhite@gamil.com",
+        DOB: "21-03-1989",
+    },
+];
+
+// GET request: Retrieve all users
+router.get("/", (req, res) => {
+    // Copy the code here
+    res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+});
+
+// GET by specific ID request: Retrieve a single user with email ID
+router.get("/:email", (req, res) => {
+    // Copy the code here
+    res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+});
+
+// POST request: Create a new user
+router.post("/", (req, res) => {
+    // Copy the code here
+    res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+});
+
+// PUT request: Update the details of a user by email ID
+router.put("/:email", (req, res) => {
+    // Copy the code here
+    res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+});
+
+// DELETE request: Delete a user by email ID
+router.delete("/:email", (req, res) => {
+    // Copy the code here
+    res.send("Yet to be implemented"); //This line is to be replaced with actual return value
+});
+
+module.exports = router;
